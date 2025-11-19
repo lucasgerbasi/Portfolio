5264cd576082961eaa18edd981060cc55c0c8bf7
Lucas Gerbasi <lucas.gerbasi1@gmail.com>
2025-11-18 22:51:58 -0300

    Small text change

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -405,7 +405,7 @@ export default function Projects() {
                     className="flex-1 min-w-[150px] bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg flex items-center justify-center transition-colors"
                   >
                     <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3z" clipRule="evenodd"></path></svg>
-                    {currentLang === 'en' ? 'Case Study' : 'Ver Estudo de Caso'}
+                    {currentLang === 'en' ? 'Case Study' : 'Estudo de Caso'}
                   </a>
                 )}
               </div>
